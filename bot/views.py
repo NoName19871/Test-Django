ee2d8bf6f489abf030d4a82ae29632350ac848eb
NoName19871 <smersh197@mail.ru>
2016-12-10 13:32:30 +0300

    master

--- bot/views.py
+++ bot/views.py
@@ -10,7 +10,7 @@ def post_list(request):
     return render(request, 'bot/post_list.html', {})
 
 
-    bot = telebot.TeleBot("301583096:AAFIj_0jfY5Xoy8WivDoieZKSczIDCGO3zg")
+bot = telebot.TeleBot("301583096:AAFIj_0jfY5Xoy8WivDoieZKSczIDCGO3zg")
 
     @bot.message_handler(content_types=["text"])
     def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
