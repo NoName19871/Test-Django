8028cc1d1dd1f8aef2a1e76316d407784d07de70
NoName19871 <smersh197@mail.ru>
2016-12-10 13:38:32 +0300

    master

--- bot/views.py
+++ bot/views.py
@@ -12,9 +12,10 @@ def post_list(request):
 
 bot = telebot.TeleBot("301583096:AAFIj_0jfY5Xoy8WivDoieZKSczIDCGO3zg")
 
-    @bot.message_handler(content_types=["text"])
-    def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-        bot.send_message(message.chat.id, message.text)
 
-    if __name__ == '__main__':
-        bot.polling(none_stop=True)
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
