d2dd56c0f30bd8be30adeeea02a9f4003b7dccca
NoName19871 <smersh197@mail.ru>
2016-12-12 23:22:45 +0300

    master

--- bot/views.py
+++ bot/views.py
@@ -10,12 +10,9 @@ def post_list(request):
     return render(request, 'bot/post_list.html', {})
 
 
-bot = telebot.TeleBot("301583096:AAFIj_0jfY5Xoy8WivDoieZKSczIDCGO3zg")
+def run_bot():
+    bot = telebot.TeleBot("301583096:AAFIj_0jfY5Xoy8WivDoieZKSczIDCGO3zg")
 
-
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
-
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    bot.send_message(message.chat.id, "Hi")
+    if __name__ == '__main__':
+        bot.polling(none_stop=True)
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('bot.urls')),
-    url(r'^telegram.me/', include('bot.urls'))
+    url(r'^bot/', include('bot.urls')),
 ]
